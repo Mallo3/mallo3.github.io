3aa9ebe566c0a511cfcb10571c64b539cbdad0ad
Mallo3 <soumallyahazra2217@gmail.com>
2023-03-15 12:01:45 +0530

    new portfolio

---
+++ src/content_option.js
@@ -0,0 +1,162 @@
+const logotext = "Soumallya";
+const meta = {
+    title: "Soumallya Hazra",
+    description: "I'm Soumallya Hazra, currently working in a MNC. Chill it's not my resume wala website.😎",
+};
+
+const introdata = {
+    title: "I’m Soumallya Hazra",
+    animated: {
+        first: "I love eating",
+        second: "I love to code cool websites",
+        third: "I like to travel too",
+    },
+    description: "I'm Soumallya Hazra, currently working in a MNC. Chill it's not my resume wala website.😎",
+    your_img_url: "../src/soumallya.jpeg",
+};
+
+const dataabout = {
+    title: "About me",
+    aboutme: "I'm Soumallya Hazra, currently working in a MNC. Chill it's not my resume wala website.😎",
+};
+const worktimeline = [{
+        jobtitle: "Software Engineer",
+        where: "LTIMindtree",
+        date: "2021 - present",
+    }
+];
+
+const skills = [{
+        name: "Java",
+        value: 80,
+    },
+    {
+        name: "AWS",
+        value: 60,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 50,
+    },
+    {
+        name: "Algo",
+        value: 70,
+    },
+];
+
+const services = [{
+        title: "Web Development Backend",
+        description: "E-commerce website's customer's data storing and offer, gamification, events processing web application",
+    },
+    {
+        title: "Portfolio Website",
+        description: "Created a portfolio website",
+    },
+    {
+        title: "Salesforce Commerce Cloud",
+        description: "E-commerce platform development using Salesforce Commerce Cloud, aka Demandware",
+    },
+];
+
+const dataportfolio = [/*{
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },*/
+    {
+        //img: "https://picsum.photos/400/700/?grayscale",
+        desctiption: "Here to visit my portfolio website",
+        link: "https://mallo3.github.io/home",
+    }
+
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "soumallyahazra2217@gmail.com",
+    //YOUR_FONE: "(555)123-4567",
+    description: "If you want to tell something, do email me",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/mallo3",
+    facebook: "https://facebook.com/profile.php?id=100009813616128",
+    //linkedin: "https://linkedin.com",
+    //twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
