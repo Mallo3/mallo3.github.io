a3b5b6da0c1fccd7cc429015cba867766a3ef6f1
Mallo3 <soumallyahazra2217@gmail.com>
2023-03-22 21:47:38 +0530

    updation

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/mallo3/mallo3.github.io.git";
+const repoURL = "https://github.com/Mallo3/mallo3.github.io.git";
 
 ghpages.publish(
   pathname,
